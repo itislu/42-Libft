5faf76b32f55b925294321cf7f8183a3921aec89
Lukas Dullinger <ldulling@c4r4p3.42vienna.com>
2023-09-26 12:46:17 +0200

    added protections against del == NULL

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,33 +6,23 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:04:22 by ldulling          #+#    #+#             */
-/*   Updated: 2023/09/24 16:23:59 by ldulling         ###   ########.fr       */
+/*   Updated: 2023/09/26 12:40:16 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* Caution: frees the list without deleting the content if del is NULL */
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*cur;
 
-	if (del != NULL)
+	if (lst != NULL && del != NULL)
 	{
 		while (*lst != NULL)
 		{
 			cur = *lst;
 			*lst = (*lst)-> next;
 			(*del)(cur -> content);
-			free(cur);
-		}
-	}
-	else
-	{
-		while (*lst != NULL)
-		{
-			cur = *lst;
-			*lst = (*lst)-> next;
 			free(cur);
 		}
 	}
--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,19 +6,17 @@
 /*   By: ldulling <ldulling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 16:04:26 by ldulling          #+#    #+#             */
-/*   Updated: 2023/09/24 16:21:50 by ldulling         ###   ########.fr       */
+/*   Updated: 2023/09/26 12:35:15 by ldulling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* Caution: frees the node without deleting the content if del is NULL */
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	if (lst != NULL)
+	if (lst != NULL && del != NULL)
 	{
-		if (del != NULL)
-			(*del)(lst -> content);
+		(*del)(lst -> content);
 		free(lst);
 	}
 	return ;
